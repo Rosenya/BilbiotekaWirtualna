903d6e1f31c22f7b080549c56d48cd395dacd2b8
Arkadia <lajluch@gmail.com>
2022-10-14 19:39:23 +0200

    Repository commit

--- src/main/java/com/ArkadiaPlocienniczak/BilbiotekaWirtualna/repository/BookRepository.java
+++ src/main/java/com/ArkadiaPlocienniczak/BilbiotekaWirtualna/repository/BookRepository.java
@@ -4,7 +4,9 @@ import com.ArkadiaPlocienniczak.BilbiotekaWirtualna.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-
+    List<Book> findByCategoryId(Long id);
 }
