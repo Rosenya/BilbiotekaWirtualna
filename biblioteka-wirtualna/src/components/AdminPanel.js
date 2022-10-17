28d37ad11ad588c9b9c6002a79927f096feafcee
Arkadia <lajluch@gmail.com>
2022-10-17 02:24:07 +0200

    frontend commit 2

---
+++ biblioteka-wirtualna/src/components/AdminPanel.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../index.css';
+
+
+import AddUser from './AddUser'
+import AddBook from './AddBook'
+
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+export default function AdminPanel(){
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+return(
+  <div>
+   
+  <Box sx={{ width: '100%', bgcolor: 'background.paper', display: 'flex'}}>
+    
+    <Tabs value={value} onChange={handleChange} centered orientation="vertical" >
+    <Tab label="Strona Główna" />
+    <Tab label="Użytkownicy" />
+    <Tab label="Książki" />
+    <Tab label="Wypożyczenia" />
+
+    </Tabs>
+    
+
+  
+</Box>
+ <AddUser/>
+ <AddBook/>
+</div>
+);
+}
